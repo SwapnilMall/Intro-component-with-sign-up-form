e86308301ca28c957d35ea2097f1865477df438f
Swapnil Mall <swapnilmall03@gmail.com>
2020-01-28 17:28:45 +0530

    Add files via upload
    made changes in index.js file and some other design realted changes .

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-var firstName = document.querySelecter(".first-name");
-alert(firstName);
+document.querySelector(".Submit-btn").addEventListener('click', submitForm);
+
+function submitForm() {
+  var firstName = document.querySelector(".first-name").value;
+  var lastName = document.querySelector(".last-name").value;
+  var email = document.getElementById("inputEmail").value;
+  var pass = document.getElementById("inputPassword").value;
+  if (firstName.length === 0) {
+    document.getElementById("first-name").style.color="hsl(0, 100%, 74%)";
+    document.querySelector(".first-name").classList.add("onSubmitWrong");
+  }
+  else{
+    document.querySelector(".first-name").classList.add("onSubmitRight");
+  }
+   if (lastName.length === 0) {
+    document.getElementById("last-name").style.color="hsl(0, 100%, 74%)";
+    document.querySelector(".last-name").classList.add("onSubmitWrong");
+  }
+  else{
+    document.querySelector(".last-name").classList.add("onSubmitRight");
+  }
+  if (email.length === 0) {
+    document.getElementById("email").style.color="hsl(0, 100%, 74%)";
+    document.querySelector(".emailA").classList.add("onSubmitWrong");
+  }
+  else{
+    document.querySelector(".emailA").classList.add("onSubmitRight");
+  }
+  if (pass.length < 6){
+    document.getElementById("password").style.color="hsl(0, 100%, 74%)";
+    document.querySelector(".passwordA").classList.add("onSubmitWrong");
+  }
+  else{
+    document.querySelector(".passwordA").classList.add("onSubmitRight");
+  }
+}
